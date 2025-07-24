7cf83b348a6d015be72765d5b1d52218c36ee2a6
wuwei <377234913@qq.com>
2025-07-24 17:30:30 +0800

    sky

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -6,13 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @Slf4j
 public class RedisConfiguration {
-
-
 
     @Bean
     public RedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -21,5 +20,12 @@ public class RedisConfiguration {
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         return redisTemplate;
+    }
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        log.info("开始创建StringRedisTemplate对象...");
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(connectionFactory);
+        return template;
     }
 }
--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -1,5 +1,8 @@
 package com.sky.controller.user;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sky.constant.StatusConstant;
 import com.sky.entity.Dish;
 import com.sky.result.Result;
@@ -7,8 +10,10 @@ import com.sky.service.DishService;
 import com.sky.vo.DishVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +27,9 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
     /**
      * 根据分类id查询菜品
      *
@@ -31,6 +39,19 @@ public class DishController {
     @GetMapping("/list")
     @ApiOperation("根据分类id查询菜品")
     public Result<List<DishVO>> list(Long categoryId) {
+        String key = "dish_" + categoryId;
+        String cachedDishes = stringRedisTemplate.opsForValue().get(key);
+        if (cachedDishes != null) {
+            // 缓存中有数据，反序列化后返回
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                List<DishVO> dishVOs = objectMapper.readValue(cachedDishes, new TypeReference<List<DishVO>>() {});
+                return Result.success(dishVOs);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("Failed to deserialize dishes from cache", e);
+            }
+        }
+
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
         dish.setStatus(StatusConstant.ENABLE);//查询起售中的菜品
